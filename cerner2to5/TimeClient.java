d12757d6b5f749946de2eaca94c428f29573241f
Surya Sengupta <surjosg@gmail.com>
2022-09-10 22:18:18 +0530

    Adding Java time client, C# time client, VB net Time client and python time client

---
+++ cerner2to5/TimeClient.java
@@ -0,0 +1,17 @@
+package cerner2to5;
+public class TimeClient {
+	public static void main(String[] args) {
+		for(String zoneId: java.time.ZoneId.getAvailableZoneIds()) {
+			new Thread(new Runnable() { public void run() {
+			try {
+				java.net.Socket socket = new java.net.Socket("localhost",49152);
+				java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.InputStreamReader(socket.getInputStream()));
+				java.io.PrintWriter out = new java.io.PrintWriter(socket.getOutputStream(), true);
+				out.println(zoneId);
+				System.out.println(zoneId + " : " + reader.readLine());
+				socket.close();	
+			}catch(Exception ex){System.out.println(ex);}
+			}}).start();
+		}
+	}
+}
---
+++ timeClient.py
@@ -0,0 +1,7 @@
+import socket
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc.connect(("localhost",49152))
+soc.send("Pacific/Honolulu\n".encode())
+data = soc.recv(1024)
+print(data.decode())
+soc.close()
